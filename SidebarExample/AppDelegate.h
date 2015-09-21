28c5aaf925fe053df820e57c1bc7e98497f6beeb
franky <franky1504@gmail.com>
2015-09-21 11:52:23 +0800

    Initial Commit

---
+++ SidebarExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SidebarExample
+//
+//  Created by i_feyuwu on 2015/9/21.
+//  Copyright © 2015年 FET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SidebarExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SidebarExample
+//
+//  Created by i_feyuwu on 2015/9/21.
+//  Copyright © 2015年 FET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
